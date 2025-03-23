fd8f1b3b11dc1870dd87974f19a343ce6e353feb
silvaronna <azka.abdillah@outlook.co.id>
2025-03-23 14:05:31 +0700

    feat: adding code to storing student data with imperative paradigm

--- W3/Imperative/studentClass.cpp
+++ W3/Imperative/studentClass.cpp
@@ -1,56 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class Student {
-private:
-    string firstname;
-    string lastname;
-    int id;
-    char grade;
+int main () {
 
-public:
-    // Constructor Default
-    Student() {
-        firstname = "";
-        lastname = "";
-        id = 0;
-        grade = ' ';
-    }
+    string firstName = "";
+    string lastName = "";
+    string id = "";
+    int grade = 0;
 
-    // Constructor dengan Parameter
-    Student(string fn, string ln, int i, char g) {
-        firstname = fn;
-        lastname = ln;
-        id = i;
-        grade = g;
-    }
+    cout << "Enter your First Name: ";
+    cin >> firstName;
+    cout << "Enter your Last Name: ";
+    cin >> lastName;
+    cout << "Enter your ID: ";
+    cin >> id;
+    cout << "Enter your Grade: ";
+    cin >> grade;
 
-    // Display function
-    void display() {
-        cout << "=======Result=======" << endl;
-        cout << "Student first name: " << firstname << endl;
-        cout << "Student last name: " << lastname << endl;
-        cout << "Student ID: " << id << endl;
-        cout << "Student Grade: " << grade << endl;
-    }
+    cout << "Name: " << firstName << " " << lastName << endl;
+    cout << "ID: " << id << endl;
+    cout << "Grade: " << grade << endl;
 
-    // Setter untuk mengisi nilai dari user
-    void setStudentData() {
-        cout << "Enter student first name: ";
-        cin >> firstname;
-        cout << "Enter student last name: ";
-        cin >> lastname;
-        cout << "Enter student ID: ";
-        cin >> id;
-        cout << "Enter student grade: ";
-        cin >> grade;
-    }
-};
-
-int main() {
-    Student data;
-    data.setStudentData();
-    data.display();
-
-    return 0;
 }
